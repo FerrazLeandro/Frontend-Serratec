4d5ac39b6178936e905a8d61587b60c9d0f7852a
Leandro Ferraz <85909017+FerrazLeandro@users.noreply.github.com>
2022-09-23 21:52:18 -0300

    Aula05

---
+++ aula05/javascript/script.js
@@ -0,0 +1,23 @@
+document.getElementById("botao").onclick = clique
+
+function clique() {
+    alert("Voce clicou");
+    console.log('Oi');
+}
+
+function somar {
+    document.getElementById()
+}
+
+
+document.getElementById("botao").onclick = function clique() {
+    var numero1 = document.getElementById("input1").value;
+    var numero2 = document.getElementById("input2").value
+    
+
+    console.log("Valor do input 1: " + numero1)
+    console.log("Valor do input 2: " + numero2)
+
+
+    console.log("A soma dos valores: " + (Number(numero1) + Number(numero2)))
+}
